d17d34c2e0fa02f580e7833263438e84e2aa8b72
Noah Sussman <noah@onemorebug.com>
2016-12-01 12:48:36 -0500

    sophisticated write function

---
+++ lib/app.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+dcube_write()
+{
+
+    eventKey=$1
+    eventValue=$2
+    relationshipKey=$3
+    relationshipValue=$4
+
+    jq -n --argjson eventArg "{\"$eventKey\": \"$eventValue\"}" --argjson relationshipArg "{\"$3\": \"$4\"}" -f lib/app.jq
+
+}
